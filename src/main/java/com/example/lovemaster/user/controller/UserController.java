52e3a2be6fa8cf660570c8d76e07fba103189095
yeonha99 <rhdqn1207@naver.com>
2021-08-07 02:15:34 +0900

    fix:Controller의 회원가입 기능 리턴 수정

--- src/main/java/com/example/lovemaster/user/controller/UserController.java
+++ src/main/java/com/example/lovemaster/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.lovemaster.user.dto.CreateUserDTO;
 import com.example.lovemaster.user.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,9 +19,8 @@ public class UserController {
 
     @ApiOperation("회원가입하는 기능")
     @PostMapping("/register")
-    public String registerPOST(CreateUserDTO userDTO, RedirectAttributes redirectAttributes){
-        userService.register(userDTO);
-        return "가입완료";
+    public ResponseEntity<String> registerPOST(CreateUserDTO userDTO, RedirectAttributes redirectAttributes){
+        return userService.register(userDTO);
     }
 
 }
